8f112b9bb6814f9866dee50bd7fb71e7c29de7b6
Juan Prieto <juanprieto1627@gmail.com>
2026-05-17 22:36:37 -0500

    MetodoGetDocumentoParaVigilante - Juan Prieto - 17/05/2026

--- src/features/users/controllers/users/users.controller.ts
+++ src/features/users/controllers/users/users.controller.ts
@@ -25,6 +25,11 @@ export class UsersController {
         return this.usersService.findOne(userId);
     }
 
+    @Get('documento/:docNumber')
+    findDocumento(@Param('docNumber', ParseIntPipe) docNumber: string){
+        return this.usersService.findByDocumento(docNumber);
+    }
+
     @Post()
     createUser(@Body() payload: CreateUserDto){
         return this.usersService.create(payload);
