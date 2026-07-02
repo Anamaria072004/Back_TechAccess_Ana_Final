34bc9823afbc2a291d03ca192c42c50bbcf26b44
jp077t-hue <tu_correo@ejemplo.com>
2026-07-02 16:38:53 -0500

    .mainn

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
